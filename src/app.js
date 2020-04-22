c22a11f69cd8bd6c43e5d39df74eedb04a5028d5
Edgar Kenyi Rojo Moreno <edgar.kenyi.rojo@gmail.com>
2020-04-22 15:13:25 -0500

    PROCESO DE MONGODB y ROUTES

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
-cons body-parser= require('body-parser');
+const bodyParser = require('body-parser');
 const  express= require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.Promise = global.Promise; 
+mongoose.connect('mongodb://localhost/rest-api-example',{useNewUrlParser: true,useUnifiedTopology: true
+}).then(db=> console.log('db is connected'))
+    .catch (err => console.log(err));
+
 
 //settings
 app.set('port', process.env.PORT||3000);
+
+//middleware
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+//routes
+
+
+//static files
+
 
 //start the server
 app.listen(app.get('port'), ()=>{
